87989181a300e6de9053872d1d989335c386a9d7
pranto saha <sahapranto32@gmail.com>
2019-03-03 13:12:33 +0600

    Create Bubble sort.cpp

---
+++ sorting algorithm/Bubble sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void buble_sort(int arra[], int a, int b){
+    for(int i=a; i<b; i++){
+        for(int j=a; j<b-i; j++){
+            if(arra[j] > arra[j+1])         // use if(arra[j] < arra[j+1]) for decreasing order;
+                swap(arra[j], arra[j+1]);
+        }
+    }
+}
+void printArray(int A[], int n){
+    for(int i=0; i<n; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arra[1000];
+    int n;
+    cout << "Enter the size of array:\n";
+    cin >> n;
+    cout << "Enter the element of array:\n";
+    for(int i=0; i<n; i++)
+        cin >> arra[i];
+    buble_sort(arra, 0, n-1);
+    printArray(arra, n);
+    return 0;
+}
